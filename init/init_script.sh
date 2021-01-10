09754c0813b18271a944498bb8d9a75ab2184c4e
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-01-10 22:27:27 +0300

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/iibrahimwt13/OlympusUserBot -b master
+        git clone https://github.com/iibrahimwt13/olympususer -b master
         save_prog "bot_clone"
     fi
     echo "DONE!!"
