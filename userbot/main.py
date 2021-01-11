fe32407246a8345f5e597a49b18c8ccab8503500
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-01-11 23:17:14 +0300

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -1,10 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Asena UserBot - Yusuf Usta
+# Olympus UserBot - Zeus Usta
 
 """ UserBot başlangıç noktası """
 import importlib
